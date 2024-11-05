d4812a37df1f7f3bf1d5fab2d2179922fd39f68d
asomania <erenkucuk2002@gmail.com>
2024-11-05 13:08:05 +0300

    CRUD opetarions added

--- ProductsAPI/Controllers/ProductController.cs
+++ ProductsAPI/Controllers/ProductController.cs
@@ -9,11 +9,48 @@ namespace ProductsAPI.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class ProductController : ControllerBase
-    {
-        [HttpGet]
-        public ActionResult<List<Products>> GetProducts()
-        {
-            return ProductsService.GetProducts();
-        }
-    }
+      {
+          [HttpGet]
+          public ActionResult<List<Products>> GetProducts()
+          {
+              return ProductsService.GetProducts();
+          }
+          [HttpGet("id")]
+          puplic ActionResult<Product> GetProduct(int id){
+            var prod = ProductsService.GetProduct(id);
+            if (prod is null){
+              return NotFound()
+            }
+            return prod;
+          }
+          [HttpPost]
+          public IActionResult AddProduct(Product prod){
+            if(prod is null){
+              return NotFound
+            }
+            ProductsService.AddProduct(prod)
+            return Ok()
+          }
+          [HttpUpdate]
+          public IActionResult UpdateProduct(int i, Product prod){
+            var product = ProductsService.GetProduct(id);
+            if(product is null)
+              return NotFound();
+             
+            prod.Id = product.Id;
+            ProductsService.UpdateProduct(prod);
+            return Ok();
+          }
+          [HttpDelete]
+          public IActionResult DeleteProduct(int i){
+            var product = ProductsService.GetProduct(id);
+            if(product is null)
+              return NotFound();
+             
+            ProductsService.DeleteProduct(i);
+            return Ok();
+          }
+
+      }
+
 }
